b531a1545d938c1bac15e86765cd8ffb70421d1a
maechabin <mitoppo@gmail.com>
2019-03-08 22:00:43 +0900

    アイコンとテキスト追加

--- src/components/index/Main.tsx
+++ src/components/index/Main.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Main(): JSX.Element {
   const style = {
@@ -12,14 +13,41 @@ export default function Main(): JSX.Element {
       flexDirection: 'column',
     } as React.CSSProperties,
     title: {
+      margin: 0,
       fontSize: '80px',
       color: '#fff',
-    },
+    } as React.CSSProperties,
+    paragraph: {
+      margin: '48px auto',
+      fontSize: '20px',
+      color: '#fff',
+    } as React.CSSProperties,
+    icon: {
+      fontSize: '64px',
+      margin: '0 40px',
+    } as React.CSSProperties,
   };
   return (
     <main>
       <div style={style.hero}>
-        <h1 style={style.title}>maechabin.com</h1>
+        <h1 style={style.title}>maechabin</h1>
+
+        <p style={style.paragraph}>
+          Welcom! I'm a web front end engineer.
+          <br />I like TypeScript, React, and Angular...
+        </p>
+
+        <div>
+          <a href="https://mae.chab.in" target="_blank" style={style.icon}>
+            <FontAwesomeIcon icon={['fab', 'wordpress']} color="#fff" />
+          </a>
+          <a href="https://github.com/maechabin" target="_blank" style={style.icon}>
+            <FontAwesomeIcon icon={['fab', 'github']} color="#fff" />
+          </a>
+          <a href="https://twitter.com/maechabin" target="_blank" style={style.icon}>
+            <FontAwesomeIcon icon={['fab', 'twitter']} color="#fff" />
+          </a>
+        </div>
       </div>
     </main>
   );
