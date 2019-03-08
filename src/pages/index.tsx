7d5cea19008b96f01edbfb6dc880024832c45c23
maechabin <mitoppo@gmail.com>
2019-03-08 22:00:13 +0900

    font-awesome導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,11 @@ import Helmet from '../components/index/Helmet';
 import Layout from '../components/layout/Layout';
 import Main from '../components/index/Main';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab);
+
 export default function Index() {
   return (
     <>
