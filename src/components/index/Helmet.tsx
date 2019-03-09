0a113470e1beb474eccc19209ae010dc010aacbd
maechabin <mitoppo@gmail.com>
2019-03-10 00:02:52 +0900

    global css 対応

--- src/components/index/Helmet.tsx
+++ src/components/index/Helmet.tsx
@@ -9,17 +9,6 @@ export default function Head(): JSX.Element {
         <meta charSet="utf-8" />
         <title>maechabin.com</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <style>
-          {`
-            body {
-              padding: 0;
-              margin: 0;
-              font-family: 'SF Pro JP', 'SF Pro Text', 'SF Pro Icons',
-                'Hiragino Kaku Gothic Pro', 'ヒラギノ角ゴ Pro W3', メイリオ, Meiryo,
-                'ＭＳ Ｐゴシック', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-            }
-          `}
-        </style>
       </Helmet>
     </>
   );
